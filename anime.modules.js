6e662e54bd78854287c23a1b8c02b9aa677d1398
Adam Hay <heyadam66@gmail.com>
2019-06-25 13:41:12 -0500

    Maybe fixed things

---
+++ anime.modules.js
@@ -0,0 +1,22 @@
+const characters = [
+
+    {
+        name: "Spike",
+        show: "Cowboby Bebop",
+    },
+    {
+        name: "L",
+        show: "Death Note",
+    },
+    {
+        name: "Goku",
+        show: "DBZ",
+    },
+    {
+        name: "Gon",
+        show: "Hunter X Hunter",
+    }
+];
+
+module.exports = characters;
+
--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const characters = [
+const moduleStuff = require('./anime.module');
 
-    {
-        name: "Spike",
-        show: "Cowboby Bebop",
-    },
-    {
-        name: "L",
-        show: "Death Note",
-    },
-    {
-        name: "Goku",
-        show: "DBZ",
-    },
-    {
-        name: "Gon",
-        show: "Hunter X Hunter",
-    }
-];
-
-for (person of characters){
+for (person of characterList){
     console.log(person.name + ' is from ' + person.show);
     
 }
+
