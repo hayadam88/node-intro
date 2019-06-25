21d9072aa0f15cd7bac801ea99e714e2b3ee8590
Adam Hay <heyadam66@gmail.com>
2019-06-25 13:17:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const characters = [
+
+    {
+        name: "Spike",
+        show: "Cowboby Bebop",
+    },
+    {
+        name: "L",
+        show: "Death Note",
+    },
+    {
+        name: "Goku",
+        show: "DBZ",
+    },
+    {
+        name: "Gon",
+        show: "Hunter X Hunter",
+    }
+];
+
+for (person of characters){
+    console.log(person.name + ' is from ' + person.show);
+    
+}
